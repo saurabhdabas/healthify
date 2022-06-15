823c58097b4f3ebf48399be833bf10e297c62bb4
Saurabh Dabas <developerdabas@gmail.com>
2022-06-15 17:17:16 +0530

    Import Hero component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-
+import HeroSection from './components/HeroSection';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <HeroSection/>
     </div>
   );
 }
