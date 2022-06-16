247186168a8500e057114e196b5395e68ad79490
Saurabh Dabas <developerdabas@gmail.com>
2022-06-16 10:38:35 +0530

    import navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import HeroSection from './components/HeroSection';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <HeroSection/>
     </div>
   );
