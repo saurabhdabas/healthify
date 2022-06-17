51609056557421c6608504b13f28f62d934d55b6
Saurabh Dabas <developerdabas@gmail.com>
2022-06-17 13:47:25 +0530

    Fetch exercise data

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import SearchExercises from './components/SearchExercises';
 import './App.css';
 
+
 function App() {
+
+  const [response, setResponse] = useState([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:8080/exercises')
+    .then((response)=>{
+      setResponse(response.data);
+    })
+  },[])
+
   return (
     <div>
       <Navbar/>
       <HeroSection/>
-      <SearchExercises/>
+      <SearchExercises response={response}/>
     </div>
   );
 }
