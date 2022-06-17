b696ea70b303521ba1188be3f237dfbd54ef5ea7
Saurabh Dabas <developerdabas@gmail.com>
2022-06-17 11:30:55 +0530

    Add videos and exercises endpoints

--- server/app.js
+++ server/app.js
@@ -4,7 +4,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const videosRouter = require('./routes/videos');
+const exercisesRouter = require('./routes/exercises');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/videos', videosRouter);
+app.use('/exercises', exercisesRouter);
 
 module.exports = app;
