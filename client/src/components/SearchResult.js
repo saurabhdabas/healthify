82a6a88c17eb2f0d7394927bb2944ce9a8c5f5d3
Saurabh Dabas <developerdabas@gmail.com>
2022-06-17 15:54:00 +0530

    Add a search result card

---
+++ client/src/components/SearchResult.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Button, Stack, Typography } from '@mui/material';
+
+const ResultCard = ({ result }) => (
+  // <Link className="result-card" to={`/result/${result.id}`}>
+  
+  <Box sx={{border:'4px solid #DC3545', marginTop:1,marginBottom:1, width:'300px', borderRadius:'4px',backgroundColor:"#FFF", cursor:'pointer'}}>
+    <img src={result.gifUrl} alt={result.name} loading="lazy" width="250px"/>
+    <Stack direction="row" display='flex' justifyContent={'space-between'} alignItems='center'>
+      <Button disabled sx={{ ml:"5px" , color: '#000', background: '#2BC48A', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+        {result.bodyPart}
+      </Button>
+      <Button disabled  sx={{ mr:"5px" ,color: '#000', background: '#FCC757', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
+        {result.target}
+      </Button>
+    </Stack>
+    <hr color="#DC3545"/>
+    <Typography color="#000" fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} mt="11px" pb="10px" fontFamily="'DM Sans', sans-serif" textTransform="capitalize" textAlign={'center'}>
+      {result.name}
+    </Typography>
+    </Box>
+  // </Link>
+);
+
+export default ResultCard;
