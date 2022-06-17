87495f8ce20f10f331e1baa096e46425cc8f4a03
Saurabh Dabas <developerdabas@gmail.com>
2022-06-17 16:43:42 +0530

    Make navbar fixed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
 const Navbar = () => (
-  <Stack direction="row" alignItems='center' justifyContent="space-around" fontFamily="'DM Sans', sans-serif" sx={{ gap: '123px', justifyContent: 'none', backgroundColor:"#0F0F0F" , height:'80px'}} >
+  <Stack direction="row" alignItems='center' justifyContent="space-around" fontFamily="'DM Sans', sans-serif" sx={{ gap: '123px', justifyContent: 'none', backgroundColor:"#0F0F0F" , height:'80px', position:'fixed', top:0, width:'100%', zIndex:'1'}} >
     {/* <Link to="/">
       <img src="../logo.png" alt="logo" style={{ width: '48px', height: '48px', margin: '0px 20px' }} />
     </Link> */}
