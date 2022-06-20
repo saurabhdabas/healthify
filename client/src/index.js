f836c0a364521e19aafed33886a7e4d472a537fd
Saurabh Dabas <developerdabas@gmail.com>
2022-06-20 12:45:53 +0530

    Add new font family

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Exercise from './components/Exercise';
+import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <App/>
-  </>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="exercises/exercise/:id" element={<Exercise/>} />
+    </Routes>
+  </Router>
 );
 
