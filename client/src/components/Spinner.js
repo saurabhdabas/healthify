96722e30deb8b92242a1e95e2fe6db8775104668
Saurabh Dabas <developerdabas@gmail.com>
2022-06-21 02:57:36 +0530

    Add Spinner component

---
+++ client/src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+import { MutatingDots } from 'react-loader-spinner';
+
+const Spinner = () => (
+  <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
+    <MutatingDots color="grey" />
+  </Stack>
+);
+
+export default Spinner;
