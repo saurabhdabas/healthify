eef93e0e589daf573475d27f44a2df7b68d91673
Saurabh Dabas <developerdabas@gmail.com>
2022-06-20 12:39:22 +0530

    Add new font

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 
 const Navbar = () => (
   <Stack direction="row" alignItems='center' justifyContent="space-around" fontFamily="'DM Sans', sans-serif" sx={{ gap: '123px', justifyContent: 'none', backgroundColor:"#0F0F0F" , height:'80px', position:'fixed', top:0, width:'100%', zIndex:'1'}} >
-    {/* <Link to="/">
-      <img src="../logo.png" alt="logo" style={{ width: '48px', height: '48px', margin: '0px 20px' }} />
-    </Link> */}
-    <img src="../logo.png" alt="logo" style={{ width: '48px', height: '48px', margin: '0px 30px' }} />
-    <a href="#exercises" style={{ textDecoration: 'none', color: '#FFFFFF' }}>Home</a>
-    <a href="#exercises" style={{ textDecoration: 'none', color: '#FFFFFF' }}>Exercises</a>
-
-    {/* <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link> */}
+    <Link to="/">
+      <img src="../logo.png" alt="logo" style={{ width: '48px', height: '48px', margin: '0px 30px' }} />
+    </Link>
+    <Link to="/" style={{textDecoration: 'none'}}>
+      <Typography variant="h6" sx={{ color: '#FFFFFF' }} textAlign="start"  fontFamily="'DM Sans', sans-serif">Home</Typography>
+    </Link>
+    <Link to="/exercises" style={{textDecoration: 'none'}}>
+      <Typography variant="h6" sx={{ color: '#FFFFFF' }} textAlign="start"  fontFamily="'DM Sans', sans-serif">Exercises</Typography>
+    </Link>
   </Stack>
 );
 
